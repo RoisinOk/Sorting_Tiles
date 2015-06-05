6cf103f2b2436b8c98b67f89a9dbff909334dd5c
RoisinOk <roisin_ok@hotmail.com>
2015-06-05 15:38:34 +0100

    Made button that calls addTile Made addTile function (to add a new draggable tile to the list)

--- Ró_SortTiles2/JS/myJavaScript.js
+++ Ró_SortTiles2/JS/myJavaScript.js
@@ -1,3 +1,23 @@
 /**
  * Created by roisinokeeffe on 05/06/2015.
  */
+var $list = $("#list1");
+var labelCount = 1;
+
+Draggable.create(".tile", {
+    type: "x,y",
+    zIndexBoost : false
+});
+
+
+function addTile(){
+
+    var element = $("<div></div>").addClass("tile").html("New Tile "+ labelCount++);
+
+    $list.append(element);
+
+    Draggable.create(element, {
+        type: "x,y",
+        zIndexBoost : false
+    });
+}
