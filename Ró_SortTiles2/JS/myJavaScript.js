43ef092485f82de7746a8c89cc976cccce662f2e
RoisinOk <roisin_ok@hotmail.com>
2015-06-06 16:14:34 +0100

    Made five list columns. Dynamically building grid on first list column.

--- Ró_SortTiles2/JS/myJavaScript.js
+++ Ró_SortTiles2/JS/myJavaScript.js
@@ -4,6 +4,8 @@
 
 //this selects the list div which contains the tiles
 var $list = $("#list1");
+//var $container =$("#container")
+
 
 //The shadows refer to the values of the box shadow on the tiles
 //shadow1 is the normal shadow
@@ -15,6 +17,29 @@ var label = 1;
 //The z-index property specifies the stack order of an element. Bigger numbers are closer.
 var zIndex = 1000;
 
+//These variables refer to the grid:
+var gridRows = 6;
+var listColumns =1;
+var tileWidth = 200;//width of tile equal to width of column
+var tileHeight = 80;
+var gutterStep = null; // Spacing between tiles
+
+
+var tiles  = $list[0].getElementsByClassName("tile");//the list of tiles
+
+//=====================================================================================
+//CREATE THE GRID
+//=====================================================================================
+
+//loop through and create the grid (a div for each cell). Feel free to tweak the variables above
+for (var i = 0; i < gridRows * listColumns; i++) {
+    y = Math.floor(i / listColumns) * tileHeight;
+    x = (i * tileWidth) % (listColumns * tileWidth);
+    $("<div/>").css({position:"absolute", border:"1px solid #454545", width:tileWidth-1, height:tileHeight-1, top:y, left:x}).prependTo($list);
+}
+
+
+
 //=====================================================================================
     //CREATE A TILE
 //=====================================================================================
@@ -23,12 +48,13 @@ var zIndex = 1000;
 function addTile(){
     //This declares a new div element with a class of tile, and gives it a HTML label of a number.
     //Then the new element is appended to the list div.
-    var element = $("<div></div>").addClass("tile").html(label++);
+    var element = $("<li></li>").addClass("tile").html(label++);
 
     // NOTE: Leave rowspan set to 1 because this demo
     // doesn't calculate different row heights
     var tile = {
         col        : null,
+        colpan     : 1,
         element    : element,
         height     : 0,
         inBounds   : true,
@@ -55,32 +81,37 @@ function addTile(){
     //I don't know why, but it might be clear later.
     Draggable.create(element, {
         type: "x,y",
-        zIndexBoost : true,
+        zIndexBoost : false,
         //Functions are called for when the tile is pressed, for when it is released.
         onPress     : onPress,
         onRelease   : onRelease,
-    });
+        //onDrag      : onDrag,
+    });//end of create draggable
+
+
 
     function onRelease() {
         //This TweenMax restores the tile to it's usual size and normal shadow
         TweenMax.to(element, 0.2, {
             autoAlpha : 1,
-            boxShadow: shadow1,
+            boxShadow : shadow1,
             scale     : 1,
+            zIndex    : ++zIndex
         });
     }//end of onRelease Function
 
-    function onPress() {
 
+
+    function onPress() {
         //This TweenMax gives the dragTile its shadow and change in size
         //The z-index makes sure that the tile that is pressed is on top of the other tiles
         TweenMax.to(element, 0.2, {
             autoAlpha : 0.75,
             boxShadow : shadow2,
             scale     : 0.95,
+            zIndex    : "+=1000"
         });
     }//end of onPress Function
-
 
 }//End of function to add a tile
 
